63eb537209a78f00d67909877b284b74d0586d1c
Sarav Ramaswamy <sarav@yahooinc.com>
2022-11-23 23:11:13 -0800

    Initial Commit

---
+++ iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Sarav Ramaswamy on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
